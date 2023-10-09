5836729c536783ef78345be7a5bbdde28dcea540
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2023-10-09 11:29:32 -0300

    1o commit local

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp2/HomeController.java
@@ -0,0 +1,19 @@
+package br.edu.ifpe.recife.gcms.WebApp2;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody; 
+ 
+@Controller 
+public class HomeController { 
+ 
+  @RequestMapping("/") 
+  public @ResponseBody String greeting() { 
+    return "Hello, Gradle"; 
+  } 
+  
+  @RequestMapping("/fulano") 
+  public @ResponseBody String greetingFulano() { 
+    return "Hello, Fulano!?!?"; 
+  } 
+}  
